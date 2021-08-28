d7bf65a163608543fbcf46c6a0a33f7cf1c44172
Jaehun Choe <32146752+jaydenchoe@users.noreply.github.com>
2021-08-28 10:40:53 +0900

    initial

---
+++ BrainHealthChecker/Scene1Page.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+
+namespace BrainHealthChecker
+{
+    public partial class Scene1Page : View
+    {
+        public Scene1Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
