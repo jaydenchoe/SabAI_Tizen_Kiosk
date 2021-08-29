80781464b4b268afba40c2b06e69dc88cdbf18b8
Jaehun Choe <32146752+jaydenchoe@users.noreply.github.com>
2021-08-29 10:32:34 +0900

    첫화면에서 두번째화면 넘어갈때 모션센싱으로도 갈수 있도록 추가
    모션센싱 추가

--- BrainHealthChecker/Scene1Page.xaml.cs
+++ BrainHealthChecker/Scene1Page.xaml.cs
@@ -1,18 +1,30 @@
 ﻿using Tizen.NUI;
 using Tizen.NUI.BaseComponents;
 using Tizen.NUI.Components;
+using Tizen.Peripheral.Gpio;
 
 namespace BrainHealthChecker
 {
     public partial class Scene1Page : View
     {
+        private const int SENSOR_MOTION_GPIO_NUMBER = 18;
+        private GpioPin motionGpio;
         public Scene1Page()
         {
             InitializeComponent();
+
+            motionGpio = new GpioPin(SENSOR_MOTION_GPIO_NUMBER, GpioPinDriveMode.Input);
+            motionGpio.ValueChanged += ChangeMotion;
         }
         private void Button_Start(object sender, ClickedEventArgs e)
         {
-           
+
+            changeScreenTo2nd();
+
+        }
+
+        private void changeScreenTo2nd()
+        {
             Window window;
             View view;
             window = NUIApplication.GetDefaultWindow();
@@ -24,16 +36,27 @@ namespace BrainHealthChecker
                 Scene1.g_pre_view.Dispose();
                 Scene1.g_pre_view = null;
             }
-      
+
             view = new Scene2Page();
             Scene1.g_pre_view = view;
             window.Add(view);
 
-          //  myView.Unparent(); // remove from the tree
-          //  myView.TouchEvent -= MyView_TouchEvent; // unscribe event handler
-          //  myView.Dispose(); // call Dispose()
-          //  myView = null; // make be unreachable for GC
-
+            //  myView.Unparent(); // remove from the tree
+            //  myView.TouchEvent -= MyView_TouchEvent; // unscribe event handler
+            //  myView.Dispose(); // call Dispose()
+            //  myView = null; // make be unreachable for GC
+        }
+        private void ChangeMotion(object sender, PinUpdatedEventArgs e)
+        {
+            
+            if (motionGpio.Read() == GpioPinValue.High)
+            {
+                changeScreenTo2nd();
+            }
+            else if (motionGpio.Read() == GpioPinValue.Low)
+            {
+                
+            }
         }
     }
 }
